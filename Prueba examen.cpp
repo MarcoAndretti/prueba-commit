b8e63ef8a59d897714f6a3e929caae3974c15c6f
MarcoAndretti <mrodriguezm32@miumg.edu.gt>
2024-03-01 10:38:21 -0600

    prueba commit 2

--- Prueba examen.cpp
+++ Prueba examen.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main() {
@@ -18,4 +18,35 @@ int main() {
     }
 
     return 0;
+}*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaración de variables para almacenar los números
+    float numero1, numero2, numero3, mayor;
+
+    // Solicitar al usuario que ingrese tres números
+    cout << "Ingrese el primer número: ";
+    cin >> numero1;
+    cout << "Ingrese el segundo número: ";
+    cin >> numero2;
+    cout << "Ingrese el tercer número: ";
+    cin >> numero3;
+
+    // Comparar los números para encontrar el mayor
+    if (numero1 > numero2 && numero1 > numero3) {
+        mayor = numero1;
+    } else if (numero2 > numero1 && numero2 > numero3) {
+        mayor = numero2;
+    } else {
+        mayor = numero3;
+    }
+
+    // Mostrar el resultado
+    cout << "El mayor de los tres números ingresados es: " << mayor << endl;
+
+    return 0;
 }
+
